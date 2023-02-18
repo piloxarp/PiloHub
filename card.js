95c6af18ce5097a8c76e6d479fa34630599702b8
piloxarp <piloxarp@gmail.com>
2023-02-18 22:30:50 +0300

    2.1

--- card.js
+++ card.js
@@ -8,49 +8,16 @@ let MINI = document.querySelector("#imgMINI");
 let OTHER = document.querySelector("#imgOTHER");
 let COLLECTION = document.querySelector("#imgCOLLECTION");
 
-
-
-function Anim1(){
-
-COMICS.src=coverCOMICS[0];
-MINI.src=coverMINI[0];
-OTHER.src=coverOTHER[0];
-COLLECTION.src=coverCOLLECTION[0];
+function Anim(frames) {
+	COMICS.src=coverCOMICS[frames];
+	MINI.src=coverMINI[frames];
+	OTHER.src=coverOTHER[frames];
+	COLLECTION.src=coverCOLLECTION[frames];
 }
-function Anim2(){
-
-COMICS.src=coverCOMICS[1];
-MINI.src=coverMINI[1];
-OTHER.src=coverOTHER[1];
-COLLECTION.src=coverCOLLECTION[1];
-}
-function Anim3(){
-
-COMICS.src=coverCOMICS[2];
-MINI.src=coverMINI[2];
-OTHER.src=coverOTHER[2];
-COLLECTION.src=coverCOLLECTION[2];
-}
-function Anim4(){
-
-COMICS.src=coverCOMICS[3];
-MINI.src=coverMINI[3];
-OTHER.src=coverOTHER[3];
-COLLECTION.src=coverCOLLECTION[3];
-}
-function Starter() {
-	let timer = 1000;
-	setTimeout(Anim1,timer);
-	setTimeout(Anim2,timer*2);
-	setTimeout(Anim3,timer*3);
-	setTimeout(Anim4,timer*4);
-}
-
 
 document.addEventListener("DOMContentLoaded", function() {
-	
-	function Start() {
-		setInterval(Starter,4000);
-	}
-   	setTimeout(Start,1000);
+	setInterval(Anim,1000,0);
+	setInterval(Anim,2000,1);
+	setInterval(Anim,3000,2);
+	setInterval(Anim,4000,3);
   });
